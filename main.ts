6a02abaaf39da36773a54e190ffb679d1b61a05e
Ungravitation <52806340+Ungravitation@users.noreply.github.com>
2020-05-19 09:28:20 +0800

    test

--- main.ts
+++ main.ts
@@ -1,7 +1,16 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+namespace b {
+
+    /**
+     * Scroll a number on the screen. If the number fits on the screen (i.e. is a single digit), do not scroll.
+     * @param interval speed of scroll; eg: 150, 100, 200, -100
+     */
+    //% block
+    //% help=basic/show-number
+    //% weight=96
+    //% blockId=device_show_number block="show|number %number" blockGap=8
+    //% async
+    //% parts="ledmatrix" interval.defl=150
+    export function showNumber123(value: number, interval?: number) {
+        
+    }
+}
